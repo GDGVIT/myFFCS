d7f47c6299c2bd47139a7403c183ddd1e0e3295b
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-10-22 01:51:14 +0530

    Sample Front End Completed

---
+++ temp_login.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h1>FFCS TIME TABLE GENERATOR</h1>
+<p>Please Enter your Registration Number to proceed.... </p>
+<br>
+<form name ='timetable' method = 'post' action ='timetable.php'>
+<label>Registration number : </label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type='text' name = 'reg_no' required>
+<input name = 'submit' value = 'proceed'>
+</form>
+</body>
+</html>
